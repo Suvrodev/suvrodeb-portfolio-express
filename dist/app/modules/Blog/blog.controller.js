15f605bbefe57652e56e117492217ed8e3de905f
Suvrodeb <suvrodevhowlader1408@gmail.com>
2025-10-10 09:17:05 +0600

    added catch Async in Blog controller

--- dist/app/modules/Blog/blog.controller.js
+++ dist/app/modules/Blog/blog.controller.js
@@ -8,91 +8,70 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.BlogControllers = void 0;
 const blog_service_1 = require("./blog.service");
+const catchAsync_1 = __importDefault(require("../../utils/catchAsync"));
 //Create Blog
-const createBlog = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const blog = req.body;
-        //will call service function to send data in db
-        const result = yield blog_service_1.BlogServices.createBlogIntoDB(blog);
-        //Send Response
-        res.status(200).json({
-            message: "Blog Added successfully",
-            success: true,
-            data: result,
-        });
-    }
-    catch (error) {
-        next(error);
-    }
-});
+const createBlog = (0, catchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const blog = req.body;
+    //will call service function to send data in db
+    const result = yield blog_service_1.BlogServices.createBlogIntoDB(blog);
+    //Send Response
+    res.status(200).json({
+        message: "Blog Added successfully",
+        success: true,
+        data: result,
+    });
+}));
 // Get All Blog
-const getAllBlog = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const result = yield blog_service_1.BlogServices.getAllBlog();
-        // Send response with the results
-        res.status(200).json({
-            message: "Blog retrieved successfully",
-            status: true,
-            data: result,
-        });
-    }
-    catch (error) {
-        next(error);
-    }
-});
+const getAllBlog = (0, catchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield blog_service_1.BlogServices.getAllBlog();
+    // Send response with the results
+    res.status(200).json({
+        message: "Blog retrieved successfully",
+        status: true,
+        data: result,
+    });
+}));
 // Get Single Blog
-const getSingleBlog = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+const getSingleBlog = (0, catchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     var _a;
-    try {
-        const blogId = (_a = req === null || req === void 0 ? void 0 : req.params) === null || _a === void 0 ? void 0 : _a.id;
-        const result = yield blog_service_1.BlogServices.getSingleBlogFromDB(blogId);
-        // Send response with the results
-        res.status(200).json({
-            message: "Blog retrieved successfully",
-            status: true,
-            data: result,
-        });
-    }
-    catch (error) {
-        next(error);
-    }
-});
+    const blogId = (_a = req === null || req === void 0 ? void 0 : req.params) === null || _a === void 0 ? void 0 : _a.id;
+    const result = yield blog_service_1.BlogServices.getSingleBlogFromDB(blogId);
+    // Send response with the results
+    res.status(200).json({
+        message: "Blog retrieved successfully",
+        status: true,
+        data: result,
+    });
+}));
 //Delete Blog
-const deleteBlog = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const blogId = req.params.id;
-        const result = yield blog_service_1.BlogServices.deleteBlogFromDB(blogId);
-        //Send Response
-        res.status(200).json({
-            message: "Blog deleted successfully ",
-            status: true,
-            data: result,
-        });
-    }
-    catch (error) {
-        next(error);
-    }
-});
+const deleteBlog = (0, catchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const blogId = req.params.id;
+    const result = yield blog_service_1.BlogServices.deleteBlogFromDB(blogId);
+    //Send Response
+    res.status(200).json({
+        message: "Blog deleted successfully ",
+        status: true,
+        data: result,
+    });
+}));
 //Update Blog
-const updateBlog = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const blogId = req.params.id;
-        const blogBody = req === null || req === void 0 ? void 0 : req.body;
-        const result = yield blog_service_1.BlogServices.updateBlogFromDB(blogId, blogBody);
-        //Send Response
-        res.status(200).json({
-            message: "Blog updated successfully From Cart",
-            status: true,
-            data: result,
-        });
-    }
-    catch (error) {
-        next(error);
-    }
-});
+const updateBlog = (0, catchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const blogId = req.params.id;
+    const blogBody = req === null || req === void 0 ? void 0 : req.body;
+    const result = yield blog_service_1.BlogServices.updateBlogFromDB(blogId, blogBody);
+    //Send Response
+    res.status(200).json({
+        message: "Blog updated successfully From Cart",
+        status: true,
+        data: result,
+    });
+}));
 exports.BlogControllers = {
     createBlog,
     getAllBlog,
--- src/app/modules/Blog/blog.controller.ts
+++ src/app/modules/Blog/blog.controller.ts
@@ -1,93 +1,72 @@
 import { NextFunction, Request, RequestHandler, Response } from "express";
 
 import { BlogServices } from "./blog.service";
+import catchAsync from "../../utils/catchAsync";
 
 //Create Blog
-const createBlog: RequestHandler = async (req, res, next) => {
-  try {
-    const blog = req.body;
+const createBlog: RequestHandler = catchAsync(async (req, res, next) => {
+  const blog = req.body;
 
-    //will call service function to send data in db
-    const result = await BlogServices.createBlogIntoDB(blog);
+  //will call service function to send data in db
+  const result = await BlogServices.createBlogIntoDB(blog);
 
-    //Send Response
-    res.status(200).json({
-      message: "Blog Added successfully",
-      success: true,
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  //Send Response
+  res.status(200).json({
+    message: "Blog Added successfully",
+    success: true,
+    data: result,
+  });
+});
 
 // Get All Blog
-const getAllBlog: RequestHandler = async (req, res, next) => {
-  try {
-    const result = await BlogServices.getAllBlog();
+const getAllBlog: RequestHandler = catchAsync(async (req, res, next) => {
+  const result = await BlogServices.getAllBlog();
 
-    // Send response with the results
-    res.status(200).json({
-      message: "Blog retrieved successfully",
-      status: true,
-      data: result,
-    });
-  } catch (error: any) {
-    next(error);
-  }
-};
-
+  // Send response with the results
+  res.status(200).json({
+    message: "Blog retrieved successfully",
+    status: true,
+    data: result,
+  });
+});
 // Get Single Blog
-const getSingleBlog: RequestHandler = async (req, res, next) => {
-  try {
-    const blogId = req?.params?.id;
-    const result = await BlogServices.getSingleBlogFromDB(blogId);
+const getSingleBlog: RequestHandler = catchAsync(async (req, res, next) => {
+  const blogId = req?.params?.id;
+  const result = await BlogServices.getSingleBlogFromDB(blogId);
 
-    // Send response with the results
-    res.status(200).json({
-      message: "Blog retrieved successfully",
-      status: true,
-      data: result,
-    });
-  } catch (error: any) {
-    next(error);
-  }
-};
-
+  // Send response with the results
+  res.status(200).json({
+    message: "Blog retrieved successfully",
+    status: true,
+    data: result,
+  });
+});
 //Delete Blog
-const deleteBlog: RequestHandler = async (req, res, next) => {
-  try {
-    const blogId = req.params.id;
-    const result = await BlogServices.deleteBlogFromDB(blogId);
+const deleteBlog: RequestHandler = catchAsync(async (req, res, next) => {
+  const blogId = req.params.id;
+  const result = await BlogServices.deleteBlogFromDB(blogId);
 
-    //Send Response
-    res.status(200).json({
-      message: "Blog deleted successfully ",
-      status: true,
-      data: result,
-    });
-  } catch (error: any) {
-    next(error);
-  }
-};
+  //Send Response
+  res.status(200).json({
+    message: "Blog deleted successfully ",
+    status: true,
+    data: result,
+  });
+});
 
 //Update Blog
-const updateBlog: RequestHandler = async (req, res, next) => {
-  try {
-    const blogId = req.params.id;
-    const blogBody = req?.body;
-    const result = await BlogServices.updateBlogFromDB(blogId, blogBody);
+const updateBlog: RequestHandler = catchAsync(async (req, res, next) => {
+  const blogId = req.params.id;
+  const blogBody = req?.body;
+  const result = await BlogServices.updateBlogFromDB(blogId, blogBody);
 
-    //Send Response
-    res.status(200).json({
-      message: "Blog updated successfully From Cart",
-      status: true,
-      data: result,
-    });
-  } catch (error: any) {
-    next(error);
-  }
-};
+  //Send Response
+  res.status(200).json({
+    message: "Blog updated successfully From Cart",
+    status: true,
+    data: result,
+  });
+});
 
 export const BlogControllers = {
   createBlog,
